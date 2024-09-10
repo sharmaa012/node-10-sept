f511f0f8dcf5d4538e92d7de4baa0e37b4c25de0
sharmaa012 <sa.bhardwaj2004@gmail.com>
2024-09-11 00:12:03 +0530

    uploading the new project

---
+++ routes/user.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const UserController = require('../controllers/UserController')
+
+router.get('/users',(req,res)=>{
+    UserController.getUser(req,res)
+})
+router.get('/user/:id',(req,res)=>{
+    UserController.getParticularUser(req,res)
+})
+module.exports = router;
